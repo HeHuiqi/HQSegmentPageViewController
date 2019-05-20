20996abf48addef102cf35e5daf4ac6c5962d878
hehuiqi <253956882@qq.com>
2019-05-20 17:31:43 +0800

    Initial Commit

---
+++ HQSegmentPageViewControllerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQSegmentPageViewControllerExample
+//
+//  Created by hehuiqi on 5/20/19.
+//  Copyright © 2019 hehuiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
